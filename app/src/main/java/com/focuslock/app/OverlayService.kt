1ae5bc136e84599117b974e9862c7082a1ad4142
m1endy <miendychannel@gmail.com>
2026-07-01 20:16:53 +0300

    Create OverlayService.kt

---
+++ app/src/main/java/com/focuslock/app/OverlayService.kt
@@ -0,0 +1,10 @@
+package com.focuslock.app
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class OverlayService : Service() {
+    override fun onBind(intent: Intent?): IBinder? = null
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_STICKY
+}
